00223124ea551be885665ad9488d1d0ba81a5cb2
Martin Schlipf <martin.schlipf@gmail.com>
2024-07-31 11:54:04 +0200

    Compute force bias

--- src/afqmc/propagator.py
+++ src/afqmc/propagator.py
@@ -6,14 +6,38 @@ import numpy.typing as npt
 @dataclass
 class Constants:
     L: npt.ArrayLike
+    number_electron: int
     number_walker: int
+
+    @property
+    def number_orbital(self):
+        return self.L.shape[0]
+
+    @property
+    def number_g(self):
+        return self.L.shape[-1]
+
+    @property
+    def shape_field(self):
+        return self.number_g, self.number_walker
+
+    @property
+    def shape_slater_det(self):
+        return self.number_walker, self.number_orbital, self.number_electron
+
+    @property
+    def L_trial(self):
+        "L projected on a trial determinant"
+        return self.L[: self.number_electron]
 
 
 class Propagator:
     def __init__(self, constants):
-        num_g = constants.L.shape[-1]
-        self._make_auxiliary_field = contract_expression(
-            "ijG,GN->ijN", constants.L, (num_g, constants.number_walker), constants=[0]
+        self._get_auxiliary_field = contract_expression(
+            "ijg,gw->ijw", constants.L, constants.shape_field, constants=[0]
+        )
+        self._get_force_bias = contract_expression(
+            "wij,jig->wg", constants.shape_slater_det, constants.L_trial, constants=[1]
         )
 
     def new_auxiliary_field(self, force_bias):
@@ -21,7 +45,10 @@ class Propagator:
             x = self.random_normal()
         else:
             x = self.random_normal() - force_bias
-        return self._make_auxiliary_field(x)
+        return self._get_auxiliary_field(x)
+
+    def force_bias(self, slater_det):
+        return self._get_force_bias(slater_det)
 
     def random_normal(self):
         pass
--- tests/test_propagation.py
+++ tests/test_propagation.py
@@ -6,10 +6,11 @@ from afqmc.propagator import Constants, Propagator
 
 def test_create_auxiliary_field_without_force_bias():
     num_g = 24
-    num_band = 4
+    num_orbital = 4
+    num_electron = 1
     num_walker = 3
-    L = np.random.random((num_band, num_band, num_g))
-    constants = Constants(L, num_walker)
+    L = np.random.random((num_orbital, num_orbital, num_g))
+    constants = Constants(L, num_electron, num_walker)
     propagator = Propagator(constants)
     x = np.random.normal(size=(num_g, num_walker))
     with patch.object(propagator, "random_normal", return_value=x) as mock:
@@ -20,10 +21,11 @@ def test_create_auxiliary_field_without_force_bias():
 
 def test_create_auxiliary_field_with_force_bias():
     num_g = 12
-    num_band = 7
+    num_orbital = 7
+    num_electron = 2
     num_walker = 4
-    L = np.random.random((num_band, num_band, num_g))
-    constants = Constants(L, num_walker)
+    L = np.random.random((num_orbital, num_orbital, num_g))
+    constants = Constants(L, num_electron, num_walker)
     propagator = Propagator(constants)
     x = np.random.normal(size=(num_g, num_walker))
     force_bias = np.random.random((num_g, num_walker))
@@ -31,3 +33,18 @@ def test_create_auxiliary_field_with_force_bias():
         auxiliary_field = propagator.new_auxiliary_field(force_bias)
         mock.assert_called_once_with()
     npt.assert_allclose(auxiliary_field, L @ (x - force_bias))
+
+
+def test_force_bias():
+    num_g = 18
+    num_orbital = 5
+    num_electron = 3
+    num_walker = 5
+    L = np.random.random((num_orbital, num_orbital, num_g))
+    constants = Constants(L, num_electron, num_walker)
+    assert constants.shape_slater_det == (num_walker, num_orbital, num_electron)
+    slater_det = np.random.random(constants.shape_slater_det)
+    propagator = Propagator(constants)
+    force_bias = propagator.force_bias(slater_det)
+    expected = np.einsum("wij,jig->wg", slater_det, L[:num_electron])
+    npt.assert_allclose(force_bias, expected)
