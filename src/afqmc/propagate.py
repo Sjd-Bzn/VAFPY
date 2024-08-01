e0bdf56fa9da9bab2c1e70f07f98c4a908092668
Martin Schlipf <martin.schlipf@gmail.com>
2024-08-01 15:04:11 +0200

    Implement full time step

--- src/afqmc/propagate.py
+++ src/afqmc/propagate.py
@@ -1,5 +1,18 @@
 from opt_einsum import contract
 
+from afqmc import determinant, field, weight
+
+
+def time_step(constants, old_slater_det, old_weight):
+    biorthogonal_det = determinant.biorthogonalize(constants, old_slater_det)
+    force_bias = field.force_bias(constants, biorthogonal_det)
+    auxiliary_field = field.auxiliary(constants, force_bias)
+    potential = field.potential(constants, auxiliary_field)
+    new_slater_det = s2(constants, potential, old_slater_det)
+    I = field.importance_sampling(auxiliary_field, force_bias)
+    new_weight = old_weight * weight.phaseless(old_slater_det, new_slater_det, I)
+    return new_slater_det, new_weight
+
 
 def taylor(constants, potential, slater_det):
     result = slater_det.copy()
