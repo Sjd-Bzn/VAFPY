Single precision try1(in propagation there is something wrong, time is same)

Code/developing/afqmc_INPUT_H2O.py
@@ -1,6 +1,6 @@
 import numpy as np
 from scipy.linalg import block_diag
-#from mpi4py import MPI
+from mpi4py import MPI
 import yaml 
 with open ("INCAR_AFQMC", "r") as f:
     inputs = yaml.safe_load(f)
@@ -8,13 +8,13 @@ with open ("INCAR_AFQMC", "r") as f:
 system = inputs.get('SYSTEM', 'Autorun')
 
 
-num_g =     inputs["NGVEC"]                                   ### from the out put file of the HF in H2 part
+num_g =  inputs["NGVEC"]                                   ### from the out put file of the HF in H2 part
 
 num_electrons_up = inputs['NEUP']
 
 num_electrons_down = inputs['NEDOWN']
 
-block_divisor = inputs['BLKDIV']
+block_divisor = inputs.get('BLKDIV', 10)
 
 num_orb =inputs["NORB"]                                             ##### from the H1.npy or H2.npy
 
@@ -24,6 +24,7 @@ en_const =  inputs["ECONST"]                            #nuclear energy from the
 
 SPIN =inputs.get("SPIN", 0)
 
+EBANDS = inputs.get("EBANDS", 0)
 
 
 MAX_RUN_TIME =inputs.get("TMAX", 184800)
@@ -38,9 +39,13 @@ NUM_WALKERS = inputs.get("NWAK", 256)
 
 NUM_STEPS =inputs.get("NSTP", 1000)
 
-#comm = MPI.COMM_WORLD
-#size = comm.Get_size()
-#NUM_WALKERS=NUM_WALKERS//size
+comm = MPI.COMM_WORLD
+size = comm.Get_size()
+NUM_WALKERS=NUM_WALKERS//size
+rank = comm.Get_rank()
+base_seed = 42
+propagator = inputs["PROPAG"]
+
 
 order_trunc =inputs.get("OTEY", 6)                                     ### exp_Taylor (matrics to ex)
 
@@ -50,7 +55,7 @@ trsh_real =inputs.get("TREL", 2000)
 
 
 
-first_cpu = True #comm.Get_rank()==0
+first_cpu =  comm.Get_rank()==0
 
 
 UPDATE_METHOD =inputs.get("UPDTM", "H")
@@ -70,7 +75,7 @@ SVD =inputs.get("SVD",False)
 if SVD==True:
     svd_trshd = inputs.get("SVDT", 1e-3)
 
-svd_trshd = inputs.get("SVDT", 1e-3)
+svd_trshd = inputs.get("SVDT", 1e-5)
 
 
 HF_TEST = inputs.get("HF", False)                                    #### HF_TEST
@@ -81,24 +86,31 @@ if HF_TEST==True:
 
 num_k =inputs.get("KPOINT", 1)                                               #### closed or open shell
 
-if num_k==8:
-    fsg = 8.08288908510240 
-    input_file_one_body_hamil = 'H1.npy'
-    input_file_two_body_hamil = 'H2.npy'
-    h_en_vasp = 24.58880813
-    ex_en_vasp = -87.45880075
-    Ec_MP2_vasp = -2.61184550
-    q_list = 'Q_list.npy'
+fsg = inputs.get("FSG", 0)
 
-if num_k==1:
-    fsg = 0.0
-    input_file_one_body_hamil = 'H1.npy'
-    input_file_two_body_hamil = 'H2.npy'
-    h_en_vasp = 0.0
-    ex_en_vasp = 0.0
-    Ec_MP2_vasp = 0.0
-    q_list = 'Q_list.npy'
-    
+input_file_one_body_hamil = 'H1.npy'
+input_file_two_body_hamil = 'H2_zip.npy'
+q_list = 'Q_list.npy'
+
+
+#if num_k==8:
+#    fsg = 8.08288908510240 
+#    input_file_one_body_hamil = 'H1.npy'
+#    input_file_two_body_hamil = 'H2.npy'
+#    h_en_vasp = 24.58880813
+#    ex_en_vasp = -87.45880075
+#    Ec_MP2_vasp = -2.61184550
+#    q_list = 'Q_list.npy'
+#
+#if num_k==1:
+#    fsg = 0.0
+#    input_file_one_body_hamil = 'H1.npy'
+#    input_file_two_body_hamil = 'H2.npy'
+#    h_en_vasp = 0.0
+#    ex_en_vasp = 0.0
+#    Ec_MP2_vasp = 0.0
+#    q_list = 'Q_list.npy'
+#    
 
 
 PSI_T_up_0 = PSI_T_down_0 = np.eye(num_orb)[:,0:num_electrons_up]
@@ -106,17 +118,13 @@ PSI_T_up_0 = PSI_T_down_0 = np.eye(num_orb)[:,0:num_electrons_up]
 PSI_T_up = PSI_T_up_0
 
 PSI_T_down = PSI_T_down_0
-
 for i in range (1,num_k):
     PSI_T_up = block_diag(PSI_T_up,PSI_T_up_0)
     PSI_T_down = block_diag(PSI_T_down,PSI_T_down_0)
 
 
-
 if SPIN==0:
-    output_file = 'AFQMC_CS_num_k'+str(num_k)+'_'+system+'_'+str(UPDATE_METHOD)+'_Reortho_'+str(REORTHO_PERIODICITY) +'_Rebal_'+str(REBAL_PERIODICITY)+'_TAU_'+str(D_TAU)+'.txt'
+    output_file = 'AFQMC_CS_num_k'+str(num_k)+'_samp_freq_'+str(SAMP_FREQ)+'_Reortho_'+str(REORTHO_PERIODICITY) +'_Rebal_'+str(REBAL_PERIODICITY)+'_TAU_'+str(D_TAU)+'_walkers_'+str(NUM_WALKERS)+'.txt'
     
 elif SPIN==1:
     output_file = 'AFQMC_SP_num_k'+str(num_k)+'_'+system+'_'+str(UPDATE_METHOD)+'_Reortho_'+str(REORTHO_PERIODICITY) +'_Rebal_'+str(REBAL_PERIODICITY)+'_TAU_'+str(D_TAU)+'.txt'
-
-

Code/developing/afqmc_RUN_kpts.py
@@ -1,10 +1,17 @@
+from mpi4py import MPI
+if MPI.COMM_WORLD.Get_rank() != 0:
+    print = lambda *arg, **kwargs: None
+
 from afqmc_funcs_kpts import *
 #first_cpu = comm.Get_rank()==0
+#import time
 from time import time
 import sys
 from os.path import exists
 log_file = 'log'
 #sys.stdout = open(log_file, "w")
+
+
 
 if first_cpu:
     print('###########################')
@@ -54,33 +61,62 @@ if first_cpu:
     print()
     print('###########################')
     print('Rading Hamiltonian...')
+
+#np.random.seed(15462)
+
 hamil = HAMILTONIAN
-h1 = np.array(read_datafile(input_file_one_body_hamil), order='C')
-#h1 = np.zeros_like(h1)
+###########################################
 
-hamil.two_body = np.array(read_datafile(input_file_two_body_hamil),dtype=np.complex64)
+####### reshape the H1.npy and H2.npy ######
+
+############################################
+### H1 double
+H1 = np.array(read_datafile(input_file_one_body_hamil))#,astype=np.complex128)
+h1 = reshape_H1(H1, num_k, num_orb)
+hamil.one_body = h1
+### H1 single
+#H1 = np.array(read_datafile(input_file_one_body_hamil))#,astype=np.complex128)
+h1_sing = h1.astype(np.complex64)#reshape_H1(H1, num_k, num_orb)
+
+### H2 double
+H2 = np.array(read_datafile(input_file_two_body_hamil))#,astype=np.complex128)
+h2 = H2#np.moveaxis(H2, 0, -1) 
+hamil.two_body = h2
+#### H2 dager
+h2_t = np.einsum('prG->rpG', hamil.two_body.conj())
+##### H2 single
+h2_sing = h2.astype(np.complex64)
+h2_sing_t = np.einsum('prG->rpG', h2_sing.conj())
+ 
 if first_cpu:
-    print('Constructing Hermitian Conjugate')
-
-h2_t = np.einsum('rpG->prG', hamil.two_body.conj())
-
-if first_cpu:
-    print('Hermitian Cojugate constructed Successfully')
+    print('Hamiltonian constructed Successfully')
     print()
     print('###########################')
-    print()
 
     print()
     print('###########################')
     print()
     print('Checking Shapes...')
     print()
-    print('hamil_two_body.shape -> ', hamil.two_body.shape)
+    print('H1.npy and H2.npy shape')
+
+    print('H1.npy shape -> ', H1.shape)
+    print('H2.npy shape -> ', H2.shape)
+
+    print( 'After reshaping ')
+    print()
+    print('h1 shape -> ', hamil.one_body.shape)
+    print('h2 shape -> ', hamil.two_body.shape)
+    print('h1 size in bytes -> ', hamil.one_body.itemsize)
+    print('h2 size in bytes -> ', hamil.two_body.itemsize)
+    print('h2_t size in bytes ->', h2_t.itemsize)
     print('Number of orbitals = ', int(hamil.two_body.shape[0]/num_k))
     print('Number of AF fields = ', hamil.two_body.shape[2] )
     print()
     print('###########################')
     print()
+
+
 
     ###################################################
     #
@@ -90,7 +126,17 @@ if first_cpu:
     print('##########################')
     print()
     print ('Reading Q_list...')
-ql = np.array(read_datafile(q_list),order='C').T
+#ql = np.array(read_datafile(q_list),order='C').T
+ql = []
+for i in range(1,num_k+1):
+    for j in range(1,num_k+1):
+        for k in range(1,num_k+1):
+            if abs(i-j)==k-1:
+                ql.append([i,j,k])
+ql = np.array(ql)
+
+
+
 if first_cpu:
     print('Q_list read successfully')
 
@@ -103,23 +149,6 @@ m_q = gen_minus_q(ql)
 if first_cpu:
     print('m_q = ', m_q)
 
-
-###################################################
-#
-#  Reshape H1
-#
-###################################################
-H1=np.zeros([num_orb*num_k,num_orb*num_k],dtype=np.complex64)
-for i in range(0,num_k):
-    H1[i*num_orb:(i+1)*num_orb,i*num_orb:(i+1)*num_orb] = h1[:,:,i]
-for i in range(0,num_k):
-    if first_cpu:
-        print('H1 check?',np.allclose(H1[i*num_orb:(i+1)*num_orb,i*num_orb:(i+1)*num_orb] ,h1[:,:,i]))
-hamil.one_body = H1
-
-print('h1 -> ', hamil.one_body.itemsize)
-print('h2 -> ', hamil.two_body.itemsize)
-print('h2_t->', h2_t.itemsize)
 
 if first_cpu:
     print()
@@ -140,18 +169,57 @@ if first_cpu:
     print('MF SUB in progress...')
 hamil_MF = HAMILTONIAN_MF
 h2_af_MF_sub = A_af_MF_sub(PSI_T_up_0,PSI_T_up,hamil.two_body,ql)
+#hamil_MF.two_body = A_af_MF_sub(PSI_T_up_0,PSI_T_up,hamil.two_body,ql)
+
 #h2_af_MF_sub_new = A_af_MF_sub_new(PSI_T_up,hamil.two_body,ql)
 #h2_af_MF_sub = np.load('H2_af.npy')
+#h2_af_MF_sub_new_t = np.einsum('rpG->prG', h2_af_MF_sub_new.conj())
 
-hamil_MF.zero_body = H_0_mf(PSI_T_up_0,PSI_T_up,hamil.two_body,h2_t,ql)
-H_zero = hamil_MF.zero_body
-print('H_zero = ', H_zero)
+L_0 = mean_field(hamil.two_body, num_electrons_up, num_orb)
+
+
+H_zero= np.einsum("g, g->", L_0, L_0 )/2/2/num_k
+print("H_0", H_zero)
+#hamil_MF.zero_body = # H_0_mf(PSI_T_up_0,PSI_T_up,hamil.two_body,h2_t,ql)
+hamil_MF.zero_body= H_zero
+print("H_zero", H_zero)
+
+
 hamil_MF.one_body = H_1_mf(PSI_T_up_0,PSI_T_up,hamil.two_body,h2_t,ql,hamil.one_body)
+#H1_exp = expm(H_1_self)
+#H1_self_half_exp = expm(H_1_self/2)
 
-'''
+
 hamil_MF.two_body_e = gen_A_e_full(h2_af_MF_sub)
 hamil_MF.two_body_o = gen_A_o_full(h2_af_MF_sub)
 #print(np.round(hamil_MF.two_body_o[1,:,:,1],4))
+
+####### single
+
+h2_af_MF_sing = A_af_MF_sub(PSI_T_up_0,PSI_T_up,h2_sing,ql)
+#hamil_MF.two_body = A_af_MF_sub(PSI_T_up_0,PSI_T_up,hamil.two_body,ql)
+
+#h2_af_MF_sub_new = A_af_MF_sub_new(PSI_T_up,hamil.two_body,ql)
+#h2_af_MF_sub = np.load('H2_af.npy')
+#h2_af_MF_sub_new_t = np.einsum('rpG->prG', h2_af_MF_sub_new.conj())
+
+L_0_sing = mean_field(h2_sing, num_electrons_up, num_orb)
+
+
+H_zero_sing= np.einsum("g, g->", L_0_sing, L_0_sing )/2/2/num_k
+print("H_0", H_zero)
+#hamil_MF.zero_body = # H_0_mf(PSI_T_up_0,PSI_T_up,hamil.two_body,h2_t,ql)
+
+
+
+hamil_MF.one_body_sing = H_1_mf(PSI_T_up_0,PSI_T_up,h2_sing,h2_sing_t,ql,h1_sing)
+#H1_exp = expm(H_1_self)
+#H1_self_half_exp = expm(H_1_self/2)
+
+
+hamil_MF.two_body_sing_e = gen_A_e_full(h2_af_MF_sing)
+hamil_MF.two_body_sing_o = gen_A_o_full(h2_af_MF_sing)
+#
 if first_cpu:
     print('Checking the averages of AeQs and AoQs after mean-filed subtraction...')
 for qc in range(1,num_k+1):
@@ -160,17 +228,24 @@ for qc in range(1,num_k+1):
         print('min(avg_Ae_Q) = ', min(avg_A_Q(PSI_T_up_0,PSI_T_up,hamil_MF.two_body_e,ql,qc)))
         print('max(avg_Ao_Q) = ', max(avg_A_Q(PSI_T_up_0,PSI_T_up,hamil_MF.two_body_o,ql,qc)))
         print('min(avg_Ao_Q) = ', min(avg_A_Q(PSI_T_up_0,PSI_T_up,hamil_MF.two_body_o,ql,qc)))
-'''
+
 if first_cpu:
     print('MF SUB completed.')
     print()
-
     print()
     print('##########################')
     print('Self-energy calculation in progress...')
 h_self = -contract('ijG,jkG->ik',hamil.two_body,h2_t)/2#/num_k
 H_1_self = -D_TAU * (hamil_MF.one_body+h_self)
+H1_self_exp = expm(H_1_self)
+H1_self_half_exp = expm(H_1_self/2)
 del(h_self)
+h_self_sing = -contract('ijG,jkG->ik',h2_sing,h2_sing_t)/2#/num_k
+H_1_self_sing = -D_TAU * (hamil_MF.one_body_sing+h_self_sing)
+H1_self_exp_sing = expm(H_1_self_sing)
+H1_self_half_exp_sing = expm(H_1_self_sing/2)
+del(h_self_sing)
+
 if first_cpu:
     print('Self-energy calculation completed.')
     print()
@@ -201,11 +276,25 @@ walkers = WALKERS
 #ALPHA_FULL_T = get_alpha_full_t(PSI_T_up,h2_t,ql,m_q)
 #ALPHA_FULL_T = get_alpha_full_t(PSI_T_up,h2_t,ql,m_q)
 #print("alpha_full_t: Memory size of numpy array in bytes:",ALPHA_FULL_T.size * ALPHA_FULL_T.itemsize/1e09)
-
 #energy = measure_E_gs(PSI_T_up,walkers.weights,walkers.mats_up,hamil.one_body,hamil.two_body,h2_t,ql,m_q,ALPHA_FULL,ALPHA_FULL_T,comm)
 energy_time_st = time()
-energy = energy_new = measure_E_gs(PSI_T_up,walkers.weights,walkers.mats_up,hamil.one_body)#,comm)#-2*num_electrons_up*num_electrons_up*num_k*fsg
+#one_body_energy = E_one(PSI_T_up, walkers.weights, walkers.mats_up, hamil.one_body)/num_k 
+#hartree = Hartree(PSI_T_up, walkers.weights, walkers.mats_up)/num_k
+#exchange = (Exchange(PSI_T_up, walkers.weights, walkers.mats_up)/num_k)
+#energy = energy_new 
+a = measure_E_gs(PSI_T_up,walkers.weights,walkers.mats_up,hamil.one_body,0)#,comm)#-2*num_electrons_up*num_electrons_up*num_k*fsg
+energy = energy_new = a[0]
+one_body_energy = E_one(PSI_T_up,walkers.weights,walkers.mats_up,hamil.one_body) 
+hartree = Hartree(PSI_T_up,walkers.weights,walkers.mats_up)
+exchange = Exchange(PSI_T_up,walkers.weights,walkers.mats_up)
+E1_vasp = EBANDS - 2 * hartree - 2* exchange
+
+
+
+
 print('energy time = ', time()-energy_time_st)
+
+
 
 #t1=time()
 #energy_test = measure_E_gs_new(PSI_T_up,walkers.weights,walkers.mats_up,hamil.one_body)#,alp,alp_t)#,comm)
@@ -213,7 +302,13 @@ print('energy time = ', time()-energy_time_st)
 
 #energy = 19.95523419043512*num_k+2*num_electrons_up*num_electrons_up*num_k*fsg*num_k 
 if first_cpu:
-    print('HF energy from new local energy routines = ', energy/num_k-2*num_electrons_up*num_electrons_up*num_k*fsg)
+    print('H1 Energy from AFQMC = ', one_body_energy)
+    print('H1 Energy from Vasp  = ', E1_vasp)
+    print('Hartree Energy       = ', hartree)
+    print('Exchange Energy      = ', exchange)
+    print('HF energy from AFQMC = ', energy)
+  #  print('HF energy from new local energy routines = ', energy_2/num_k-2*num_electrons_up*num_electrons_up*num_k*fsg)
+
 
 if HF_TEST==True:
     if first_cpu:
@@ -233,23 +328,20 @@ if HF_TEST==True:
         print()
         print(' tau                e_Hf                       d_e',)
         print('===================================================')
-
-    HF_TEST_H_1_self = -HF_TEST_tau * (hamil_MF.one_body+h_self)
+    HF_TEST_H_1_self =-HF_TEST_tau * (h_self +hamil_MF.one_body )
     for mul in range(0,HF_TEST_steps):
-        for i in range(0,NUM_WALKERS):
-            walkers.mats_up[i] = np.dot(propagator_fp(hamil_MF, HF_TEST_H_1_self,HF_TEST_tau,0),walkers.mats_up[i])
         val=np.array([])
-        for walker_mat in walkers.mats_up:
-            out = average_Hamil(hamil_MF, HF_TEST_H_1_self, PSI_T_up, walker_mat, NUM_WALKERS, HF_TEST_tau, 0, mul+2)
+        for i in range(0,NUM_WALKERS):
+        #    walkers.mats_up[i] = np.dot(propagator_fp(hamil_MF, HF_TEST_H_1_self,HF_TEST_tau,0),walkers.mats_up[i])
+        #for walker_mat in walkers.mats_up:
+            #out = average_Hamil(hamil_MF, hamil_MF.one_body, PSI_T_up, walkers.mats_up[i], NUM_WALKERS, HF_TEST_tau, 0, 1)
+            out = average_Hamil(hamil_MF, HF_TEST_H_1_self, PSI_T_up, walkers.mats_up[i], NUM_WALKERS, HF_TEST_tau, 0, 1)
             val=np.append(val,out)
         res = np.mean(val)
         std_err = np.std(val)/np.sqrt(len(val)-1)
         txt = str(HF_TEST_tau*(mul+1)) + '\t' + str(res/num_k-2*num_k*num_electrons_up*num_electrons_up*fsg) + '\t' + str(std_err) + '\n'
         print (txt)
 
-energy_new = energy
-e_hf=energy
-e_hf_1 = e_hf
 j = 1
 wr_name='WALKERS.npy'
 wt_name='WEIGHT.npy'
@@ -257,16 +349,19 @@ file_exists = exists(wr_name)
 if file_exists:
     walkers.mats_up=np.load(wr_name)
     walkers.weights=np.load(wt_name)
-    energy_new = measure_E_gs(PSI_T_up,walkers.weights,walkers.mats_up,hamil.one_body)
+    a = measure_E_gs(PSI_T_up,walkers.weights,walkers.mats_up,hamil.one_body,0)#,comm)#-2*num_electrons_up*num_electrons_up*num_k*fsg
+    energy_new = a[0]
     if first_cpu:
-        print(energy_new/num_k-2*num_k*num_electrons_up*num_electrons_up*fsg)
+        print(energy_new)#-2*num_k*num_electrons_up*num_electrons_up*fsg)
         #print('HF energy from test local energy routines = ', energy_test/num_k-2*num_electrons_up*num_electrons_up*num_k*fsg)
-e_hf=energy_new
+#energy_new = energy
+e_hf = energy_new
+b = a[1]
+c = a[2]
 if file_exists:
     file_out = open(output_file,"a")
 else:
     file_out = open(output_file,"w")
-
 if first_cpu:
     print()
     print('###########################')
@@ -275,21 +370,28 @@ if first_cpu:
     print()
 num_rare = 0
 start_time = time()
-update_method = set_update_method(UPDATE_METHOD)
+#update_method = set_update_method(UPDATE_METHOD)
 while (j<NUM_STEPS+1):
-    old_walkers_up = walkers.mats_up
-    old_weights = walkers.weights
+    walkers_up_sing = walkers.mats_up.astype(np.complex64)
+    weights_sing = walkers.weights.astype(np.complex64)
     update_time_st=time()
-    walkers.mats_up,walkers.weights = update_walker(PSI_T_up_0,PSI_T_up,walkers.mats_up,walkers.weights,ql,H_zero,H_1_self,D_TAU,e_hf,update_method)
-    #print('walkers.mats_up,walkers.weights ', walkers.mats_up.itemsize,walkers.weights.itemsize)
-    #print('walkers.weights = ', walkers.weights)
-    #print('np.sum(walkers.weights) = ', np.sum(walkers.weights))
-    walkers.weights=walkers.weights/np.sum(walkers.weights)
-    #print('walkers.weights = ', walkers.weights)
-    #print('np.sum(walkers.weights) = ', np.sum(walkers.weights))
-    print('update time = ', time()-update_time_st)
-    #for l in range (0, NUM_WALKERS):
-        #walkers.mats_up[l] = reortho_qr(walkers.mats_up[l])
+    #walkers.mats_up,walkers.weights = update_walker(PSI_T_up_0,PSI_T_up,walkers.mats_up,walkers.weights,ql,0,hamil.one_body,D_TAU,0,update_method)
+    #a=  measure_E_gs(PSI_T_up,walkers.weights,walkers.mats_up,hamil.one_body,0)#,comm)#-2*num_electrons_up*num_electrons_up*num_k*fsg
+    #energy_new= a[0]
+    #print('energy before update', energy_new)
+    #print('walkers before', walkers.mats_up.dtype) 
+    #print('weights before', walkers.weights.dtype) 
+    start_time = time()
+    walkers.mats_up_sing,walkers.weights_sing = update_hyb_sing(PSI_T_up_0, PSI_T_up,walkers_up_sing,weights_sing,ql,0,h1_sing,D_TAU,0,H1_self_half_exp_sing,propagator)
+    time_sing = time() - start_time
+    print(f"prop sing time: {time_sing:.6f} second")
+    s_time = time()
+    walkers.mats_up_double,walkers.weights_double = update_hyb_double(PSI_T_up_0, PSI_T_up,walkers.mats_up,walkers.weights,ql,0,hamil.one_body,D_TAU,0,H1_self_half_exp,propagator)
+    time_double = time() - s_time
+    print(f"prop double time: {time_double:.6f} second")
+    #print('wpropeights after', walkers.weights.dtype)
+    #print('walkers after', walkers.mats_up.dtype) 
+    #walkers.weights=walkers.weights/np.sum(walkers.weights)
     if j%CHECK_PERIODICITY==0:
         np.save(wr_name,walkers.mats_up)
         np.save(wt_name,walkers.weights)
@@ -298,23 +400,29 @@ while (j<NUM_STEPS+1):
     #walkers.mats_up,walkers.weights = update_fp(PSI_T_up_0,PSI_T_up,walkers.mats_up,walkers.weights,ql,hamil_MF,H_1_self,D_TAU,e_hf,hamil,h2_t,ALPHA_E,ALPHA_O)
     #energy_new = measure_E_gs(PSI_T_up,walkers.weights,walkers.mats_up,hamil.one_body,hamil.two_body,h2_t,ql,m_q,ALPHA_FULL,ALPHA_FULL_T,comm)#-2*num_electrons_up*num_electrons_up*num_k*fsg
     if j%SAMP_FREQ==0:
-        energy_time_st = time()
-        energy_new = measure_E_gs(PSI_T_up,walkers.weights,walkers.mats_up,hamil.one_body)#,comm)#-2*num_electrons_up*num_electrons_up*num_k*fsg
-        print('energy time = ', time()-energy_time_st)
-        #t1=time()
-        #energy_new_test = measure_E_gs_new(PSI_T_up,walkers.weights,walkers.mats_up,hamil.one_body)#alp,alp_t)#,comm)#-2*num_electrons_up*num_electrons_up*num_k*fsg
-        #print('energy test time = ', time()-t1)
-        #print('???',energy_new,energy_new_test)
-
-    if abs(energy.imag)<abs(trsh_imag) and abs(energy.real-e_hf.real)<abs(trsh_real) :#ratio*(energy.real+en_const))):# and (abs((energy.real-e_hf.real)/(en_const+e_hf.real))<MAX_ACC_VAL):
-        e_hf=e_hf*j+energy_new
-        e_hf = e_hf/(j+1)
+        t1=time()
+        a=  measure_E_gs(PSI_T_up,walkers.weights,walkers.mats_up,hamil.one_body,e_hf)#,comm)#-2*num_electrons_up*num_electrons_up*num_k*fsg
+        t=  measure_E_gs(PSI_T_up,walkers.weights_sing,walkers.mats_up_sing,h1_sing,e_hf)#
+        exit() 
+        energy_new= a[0]
+        b = a[1]
+        c = a[2]
+        sp_mean = a[3]
+        sp = a[4] 
+#        e_one_new =  E_one(PSI_T_up,walkers.weights,walkers.mats_up,hamil.one_body)
+#        hartree_new = Hartree(PSI_T_up,walkers.weights,walkers.mats_up)
+#        exchange_new = Exchange(PSI_T_up,walkers.weights,walkers.mats_up)
+#
+        print('energy test time = ', time()-t1)
+    
+    if abs(energy.imag)<abs(trsh_imag) and abs(energy.real-e_hf.real)<abs(trsh_real): #ratio*(energy.real+en_const))):# and (abs((energy.real-e_hf.real)/(en_const+e_hf.real))<MAX_ACC_VAL):
+        e_hf = e_hf *j +energy_new
+        e_hf = e_hf /(j+1)
         if first_cpu:
-            print(j*D_TAU, e_hf/num_k-2*num_k*num_electrons_up*num_electrons_up*fsg)
-            txt = str(j*D_TAU) + '\t' + str(energy_new.real/num_k-2*num_k*num_electrons_up*num_electrons_up*fsg) + '\t' + str(energy_new.imag) + '\n'
+            print(j*D_TAU, e_hf)#, e_one_new, hartree_new, exchange_new)
+            txt = str(j*D_TAU) + '\t' + str(energy_new.real) + '\t' + str(energy_new.imag) + '\t' + str(b.real) + '\t' + str(c.real) + '\t' + str(sp_mean.real) + '\t' + str(sp.real)  + '\n' #+ str(exchange_new.real) + '\n'
             file_out.write(txt)
             print()
-        #print('NUM_WALKERS = ', NUM_WALKERS)
         if j%REORTHO_PERIODICITY == 0:
             for i in range (0, NUM_WALKERS):
                 walkers.mats_up[i] = reortho_qr(walkers.mats_up[i])
@@ -341,6 +449,14 @@ while (j<NUM_STEPS+1):
         rebalanced_weights_indices = rebalance_comb(walkers.weights)
         walkers.mats_up = walkers.mats_up[rebalanced_weights_indices]
         walkers.weights = init_walkers_weights(NUM_WALKERS)
+
+
+# Check if the simulation has exceeded the maximum runtime (TMAX)
+#    if time() - start_time > MAX_RUN_TIME:
+#        print(f"Simulation stopped early at step {step} due to reaching TMAX.")
+#        break
+
+
 file_out.close()
     
 if first_cpu:
@@ -356,7 +472,7 @@ data1=np.loadtxt(file_out)
 data = data1[:,[1]]
 st = int(len(data)*EQUILIBRATION)
 en = len(data)
-a = blockAverage(data[st:en])
+a = blockAverage(data[st:en], block_divisor)
 if first_cpu:
    print()
    print('Calculating ground state energy...')
@@ -368,18 +484,20 @@ file_out.close()
 if first_cpu:
    print()
 
-# Output to the additional file
-#E_gs = "test.txt"
-#with open(E_gs, 'w') as gs_file:
-#    if first_cpu:
-#        gs_file.write("\nCalculating ground state energy...\n\n")
-#        gs_file.write(f'E_gs_afqmc = {np.mean(data[st:en])} ± {np.sqrt(max(a[1]))}\n')
-#        gs_file.write(f'Block mean = {a[2]}\n')
-#        gs_file.write(f'Block size = {a[0]}\n')
-#        gs_file.write(f'Variance = {a[1]}\n')
-#
+
+if first_cpu:
+    with open('output.txt', 'a') as file_out:
+        print(file=file_out)
+        print('Calculating ground state energy...', file=file_out)
+        print(file=file_out)
+        print('E_gs_afqmc = ', np.mean(data[st:en]), '+-', np.sqrt(max(a[1])), file=file_out)
+        print('total runtime = ', -start_time+time(), file=file_out)
+ 
+
+
+
 #if first_cpu:
-   print()
+#   print()
 
 #v, blockVar, blockMean = blockAverage(data[st:en])
 #import matplotlib.pyplot as plt

Code/developing/afqmc_alpha.py
@@ -1,3 +1,7 @@
+from mpi4py import MPI
+if MPI.COMM_WORLD.Get_rank() != 0:
+    print = lambda *arg, **kwargs: None
+
 import numpy as np
 from dataclasses import dataclass
 from afqmc_INPUT_H2O import *
@@ -110,10 +114,23 @@ def get_q_list(q_list,q_selected):
 
 @dataclass
 class HAMILTONIAN_MF:
-    zero_body: np.complex64
-    one_body: np.complex64
-    two_body_e: np.complex64
-    two_body_o: np.complex64
+    zero_body: np.complex128
+    one_body: np.complex128
+    two_body_e: np.complex128
+    two_body_o: np.complex128
+
+def mean_field( h2, num_elec, num_band):
+    mask = np.array(num_k * (num_elec * [True] + (num_band - num_elec) * [False]))
+    m = np.sum(h2[mask,mask], axis=0)
+    #m = np.einsum("iig->g", H2[mask][:,mask])
+    #m = np.linalg.det( h2[:num_elec, : num_elec].T)
+    return m
+
+
+
+
+
+
 
 def H_0_mf(trial_0,trial,h2,h2_dagger,q_list):
     '''
@@ -195,32 +212,54 @@ def overlap(left_slater_det, right_slater_det):
 
 hamil = HAMILTONIAN
 h1 = np.array(read_datafile(input_file_one_body_hamil),order='C')
-H1=np.zeros([num_orb*num_k,num_orb*num_k],dtype=np.complex64)
+H1=np.zeros([num_orb*num_k,num_orb*num_k],dtype=np.complex128)
 for i in range(0,num_k):
     H1[i*num_orb:(i+1)*num_orb,i*num_orb:(i+1)*num_orb] = h1[:,:,i]
 hamil.one_body = H1
 print('h1 -> ', hamil.one_body.itemsize)
+h1_sing = H1.astype(np.complex64)
 del(h1)
 del(H1)
-hamil.two_body = np.array(read_datafile(input_file_two_body_hamil),dtype=np.complex64)
-print('h2 -> ', hamil.two_body.itemsize)
-h2_t = np.einsum('rpG->prG', hamil.two_body.conj())
-print('h2_t -> ', h2_t.itemsize)
-ql = np.array(read_datafile(q_list),order='C').T
+#ql = np.array(read_datafile(q_list),order='C').T
+ql = []
+for i in range(1,num_k+1):
+    for j in range(1,num_k+1):
+        for k in range(1,num_k+1):
+            if abs(i-j)==k-1:
+                ql.append([i,j,k])
+ql = np.array(ql)
+
+
+
+H2 = np.array(read_datafile(input_file_two_body_hamil),dtype=np.complex128)
+h2 = H2#np.moveaxis(H2, 0, -1) 
+hamil.two_body = h2
+h2_t = np.einsum('prG->rpG', hamil.two_body.conj())
+h2_sing = h2.astype(np.complex64)
+h2_t_sing = np.einsum('prG->rpG', h2_sing.conj())
+ 
+#ql = np.array(read_datafile(q_list),order='C').T
 m_q = gen_minus_q(ql)
 print('Hamiltonian done')
 
 #alpha_mul_new = get_alpha_mul(PSI_T_up,hamil.two_body,h2_t,ql,m_q)
 
-alp = get_alpha_full(PSI_T_up,hamil.two_body,ql)
-np.save('alpha_intl.npy',alp)
-print('alpha done')
-alp_t = get_alpha_full_t(PSI_T_up,h2_t,ql,m_q)
-print('alpha_t done')
-np.save('alpha_t_intl.npy',alp_t)
+#alp = get_alpha_full(PSI_T_up,hamil.two_body,ql)
+#np.save('alpha_intl.npy',alp)
+#print('alpha done')
+#alp_t = get_alpha_full_t(PSI_T_up,h2_t,ql,m_q)
+print('PSI_T_up', PSI_T_up.shape)
+print('hamil_two ',hamil.two_body.shape)
+alp = np.einsum('pi, prg -> irg',PSI_T_up , hamil.two_body)
+alp_t = np.einsum('pi, rpg -> irg', PSI_T_up, hamil.two_body.conj())
+alp_sing = np.einsum('pi, prg -> irg',PSI_T_up , h2_sing)
+alp_t_sing = np.einsum('pi, rpg -> irg', PSI_T_up, h2_sing.conj())
+
+#print('alpha_t done')
+#np.save('alpha_t.npy',alp_t)
 #alp = np.load('alpha.npy')
 #alp_t = np.load('alpha_t.npy')
-print('alps -> ', alp.itemsize, alp_t.itemsize)
+#print('alps -> ', alp.itemsize, alp_t.itemsize)
 print('alps done')
 
 #alpha_mul_old = contract('qipG,qjrG->ipjr',ALPHA_FULL,ALPHA_FULL_T)
@@ -231,29 +270,57 @@ print('alps done')
 #print()
 
 hamil_MF = HAMILTONIAN_MF
-st_time=time()
+#st_time=time()
 h2_af_MF_sub = A_af_MF_sub(PSI_T_up_0,PSI_T_up,hamil.two_body,ql)
-#h2_af_MF_sub = np.load('H2_af.npy') #A_af_MF_sub(PSI_T_up_0,PSI_T_up,hamil.two_body,ql)
-#np.save('H2_af_intl.npy',h2_af_MF_sub)
-print('HF time = ', time()-st_time)
-print('HF done')
-st_time=time()
-hamil_MF.zero_body = H_0_mf(PSI_T_up_0,PSI_T_up,hamil.two_body,h2_t,ql)
-print('h0 = ', hamil_MF.zero_body)
+##h2_af_MF_sub = np.load('H2_af.npy') #A_af_MF_sub(PSI_T_up_0,PSI_T_up,hamil.two_body,ql)
+##np.save('H2_af_intl.npy',h2_af_MF_sub)
+#print('HF time = ', time()-st_time)
+#print('HF done')
+#st_time=time()
+
+#hamil_MF.zero_body = H_0_mf(PSI_T_up_0,PSI_T_up,hamil.two_body,h2_t,ql)
+hamil_MF.zero_body = mean_field(h2, num_electrons_up, num_orb)
+#
+#print('h0 = ', hamil_MF.zero_body)
 hamil_MF.one_body = H_1_mf(PSI_T_up_0,PSI_T_up,hamil.two_body,h2_t,ql,hamil.one_body)
 hamil_MF.two_body_e = gen_A_e_full(h2_af_MF_sub)
+
 hamil_MF.two_body_o = gen_A_o_full(h2_af_MF_sub)
-print('h_e -> ', hamil_MF.two_body_e.itemsize, hamil_MF.two_body_o.itemsize)
-del(hamil)
+
+
+h2_af_MF_sing = A_af_MF_sub(PSI_T_up_0,PSI_T_up,h2_sing,ql)
+##h2_af_MF_sub = np.load('H2_af.npy') #A_af_MF_sub(PSI_T_up_0,PSI_T_up,hamil.two_body,ql)
+##np.save('H2_af_intl.npy',h2_af_MF_sub)
+#print('HF time = ', time()-st_time)
+#print('HF done')
+#st_time=time()
+
+#hamil_MF.zero_body = H_0_mf(PSI_T_up_0,PSI_T_up,hamil.two_body,h2_t,ql)
+hamil_MF.zero_body_sing = mean_field(h2_sing, num_electrons_up, num_orb)
+#
+#print('h0 = ', hamil_MF.zero_body)
+hamil_MF.one_body_sing = H_1_mf(PSI_T_up_0,PSI_T_up,h2_sing,h2_t_sing,ql,h1_sing)
+hamil_MF.two_body_e_sing = gen_A_e_full(h2_af_MF_sing)
+
+hamil_MF.two_body_o_sing = gen_A_o_full(h2_af_MF_sing)
+
+
+
+#print('h_e -> ', hamil_MF.two_body_e.itemsize, hamil_MF.two_body_o.itemsize)
+#del(hamil)
 
 ALPHA_E = contract('ip,prG->irG',PSI_T_up.T,hamil_MF.two_body_e)
 #np.save('a_e_intl.npy',ALPHA_E)
 ALPHA_O = contract('ip,prG->irG',PSI_T_up.T,hamil_MF.two_body_o)
+ALPHA_E_sing = contract('ip,prG->irG',PSI_T_up.T,hamil_MF.two_body_e_sing)
+#np.save('a_e_intl.npy',ALPHA_E)
+ALPHA_O_sing = contract('ip,prG->irG',PSI_T_up.T,hamil_MF.two_body_o_sing)
+
 #np.save('a_o_intl.npy',ALPHA_O)
 #ALPHA_E = np.load('a_e.npy') #np.einsum('ip,prG->irG',PSI_T_up.T,hamil_MF.two_body_e)
 #ALPHA_O = np.load('a_o.npy') #np.einsum('ip,prG->irG',PSI_T_up.T,hamil_MF.two_body_o)
-print('even odd time intl = ', time()-st_time)
-print('even odd done')
+#print('even odd time intl = ', time()-st_time)
+#print('even odd done')
 #st_time=time()
 #ALPHAs_MUL = np.load('aa.npy')#get_alpha_mul(PSI_T_up,hamil.two_body,h2_t,ql,m_q)
 #print()
@@ -269,20 +336,29 @@ print('even odd done')
 #expr_hart = contract_expression('Nri,qirG,Npj,qjpG->N',(NUM_WALKERS,num_orb*num_k,num_electrons_up*num_k),ALPHA_FULL,(NUM_WALKERS,num_orb*num_k,num_electrons_up*num_k),ALPHA_FULL_T,constants=[1,3],optimize='greedy')
 #st_time=time()
 #expr_hart_new = contract_expression('Nri,Npj,irjp->N',(NUM_WALKERS,num_orb*num_k,num_electrons_up*num_k),(NUM_WALKERS,num_orb*num_k,num_electrons_up*num_k),ALPHAs_MUL,constants=[2],optimize='greedy')
+#expr_hart_new_new = contract_expression('Nri,irG,Npj,jpG->N',(NUM_WALKERS,num_orb*num_k,num_electrons_up*num_k),alp,(NUM_WALKERS,num_orb*num_k,num_electrons_up*num_k),alp_t,constants=[1,3],optimize='greedy')
 #print('expr_hart_new time = ', time()-st_time)
 expr_h2_e = contract_expression('ijG,GN->ijN',hamil_MF.two_body_e,(num_g,NUM_WALKERS),constants=[0],optimize='greedy')
 expr_h2_o = contract_expression('ijG,GN->ijN',hamil_MF.two_body_o,(num_g,NUM_WALKERS),constants=[0],optimize='greedy')
+
+expr_h2_e_sing = contract_expression('ijG,GN->ijN',hamil_MF.two_body_e_sing,(num_g,NUM_WALKERS),constants=[0],optimize='greedy')
+expr_h2_o_sing = contract_expression('ijG,GN->ijN',hamil_MF.two_body_o_sing,(num_g,NUM_WALKERS),constants=[0],optimize='greedy')
+
 expr_fb_e = contract_expression('Nri,irG->NG',(NUM_WALKERS,num_orb*num_k,num_electrons_up*num_k),ALPHA_E,constants=[1],optimize='greedy')
 expr_fb_o = contract_expression('Nri,irG->NG',(NUM_WALKERS,num_orb*num_k,num_electrons_up*num_k),ALPHA_O,constants=[1],optimize='greedy')
 
+expr_fb_e_sing = contract_expression('Nri,irG->NG',(NUM_WALKERS,num_orb*num_k,num_electrons_up*num_k),ALPHA_E_sing,constants=[1],optimize='greedy')
+expr_fb_o_sing = contract_expression('Nri,irG->NG',(NUM_WALKERS,num_orb*num_k,num_electrons_up*num_k),ALPHA_O_sing,constants=[1],optimize='greedy')
+
+
 expr_exch_new_new = contract_expression('Nri,jrG,Npj,ipG->N',(NUM_WALKERS,num_orb*num_k,num_electrons_up*num_k),alp,(NUM_WALKERS,num_orb*num_k,num_electrons_up*num_k),alp_t,constants=[1,3],optimize='greedy')
+expr_exch_sp = contract_expression('Nri,jrG,Npj,ipG->N',(NUM_WALKERS,num_orb*num_k,num_electrons_up*num_k),alp.astype(np.complex64),(NUM_WALKERS,num_orb*num_k,num_electrons_up*num_k),alp_t.astype(np.complex64),constants=[1,3],optimize='greedy')
+
+
 expr_hart_new_new = contract_expression('Nri,irG,Npj,jpG->N',(NUM_WALKERS,num_orb*num_k,num_electrons_up*num_k),alp,(NUM_WALKERS,num_orb*num_k,num_electrons_up*num_k),alp_t,constants=[1,3],optimize='greedy')
-
-
-
+expr_hart_sp = contract_expression('Nri,irG,Npj,jpG->N',(NUM_WALKERS,num_orb*num_k,num_electrons_up*num_k),alp.astype(np.complex64),(NUM_WALKERS,num_orb*num_k,num_electrons_up*num_k),alp_t.astype(np.complex64),constants=[1,3],optimize='greedy')
+      
 #exp_f = contract_expression('Nri,qirG->NqG',(NUM_WALKERS,num_orb*num_k,num_electrons_up*num_k),ALPHA_FULL,constants=[1])
 #exp_ft = contract_expression('Nri,qirG->NqG',(NUM_WALKERS,num_orb*num_k,num_electrons_up*num_k),ALPHA_FULL_T,constants=[1])
-#expr_exch = contract_expression('Nri,jrG,Npj,ipG->N', (NUM_WALKERS,num_orb*num_k,num_electrons_up*num_k), (num_electrons_up*num_k,num_orb*num_k,num_g),(NUM_WALKERS,num_orb*num_k,num_electrons_up*num_k),(num_electrons_up*num_k,num_orb*num_k,num_g))#,optimize='greedy')
+#expr_exch = contract_expression('Nri,jrG,Npj,ipG->N', (NUM_WALKERS,num_orb*num_k,num_electrons_up*num_k), (num_electrons_up*num_k,num_orb*num_k,),(NUM_WALKERS,num_orb*num_k,num_electrons_up*num_k),(num_electrons_up*num_k,num_orb*num_k,num_g))#,optimize='greedy')
 #expr_exch_2 = contract_expression('Nri,qjrG,Npj,qipG->N',(NUM_WALKERS,num_orb*num_k,num_electrons_up*num_k),(num_k,num_electrons_up*num_k,num_orb*num_k,num_g),(NUM_WALKERS,num_orb*num_k,num_electrons_up*num_k),(num_k,num_electrons_up*num_k,num_orb*num_k,num_g))
-
-