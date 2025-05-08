2140f81ff1bd8749c6a57c28436741e00ce2c9fb
Sjd_Bzn <sajjadbazrafshan6@gmail.com>
2025-05-08 17:10:34 +0200

    regression test (test_diamond)

--- Code/martin/test_diamond.py
+++ Code/martin/test_diamond.py
@@ -1,10 +1,23 @@
 
 
 #@pytest.mark.skip
+import os
+import subprocess
+
 def test_diamond():
-    import afqmc_RUN_kpts 
-    with open("outcar.txt", "r") as current:
+    os.environ["REGRESSION_TEST"] = "1"
+
+    # First run: generate outcar.txt and move to reference
+    subprocess.run(["python", "afqmc_RUN_kpts.py"], check=True)
+    os.rename("outcar.txt", "outcar_ref.txt")
+
+    # Second run: generate new outcar.txt
+    subprocess.run(["python", "afqmc_RUN_kpts.py"], check=True)
+
+    # Compare output files
+    with open("outcar.txt", "r") as current, open("outcar_ref.txt", "r") as reference:
         current_lines = current.readlines()
-    with open("outcar_ref.txt", "r") as reference:
         reference_lines = reference.readlines()
-    assert current_lines == reference_lines
+
+    assert current_lines == reference_lines, "Outputs do not match!"
+
