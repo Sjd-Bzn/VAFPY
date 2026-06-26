5ced34727622a2e6ef8f1b589ec79139a3ae4924
Sjd_Bzn <sajjadbazrafshan6@gmail.com>
2026-06-26 11:00:21 +0200

    Set JAX precision to single from FT32

--- source/vafpy_v1/functions.py
+++ source/vafpy_v1/functions.py
@@ -3,6 +3,7 @@ import types
 from time import time
 import numpy as np
 import jax
+jax.config.update("jax_default_matmul_precision", "float32")
 import jax.numpy as jnp
 import scipy
 from mpi4py import MPI
@@ -581,7 +582,7 @@ def obtain_H1(config):
     Reshape H1 (H1.npy shape is num_orb, num_orb, num_k
     we implement the k_points to the H1)
     """
-    input_h1 = config.backend.load("H1.npy").astype(config.complex_type)
+    input_h1 = config.backend.load("H1_svd.npy").astype(config.complex_type)
     output_h1 = config.backend.moveaxis(input_h1, -1, 0)
     return config.backend.block_diag(*output_h1)
 
